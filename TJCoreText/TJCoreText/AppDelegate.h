544f434b14c49b75a1aed8122c76087e922f30ba
wangshaojie <sj1060820555@163.com>
2016-06-30 15:12:04 +0800

    new brench

---
+++ TJCoreText/TJCoreText/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TJCoreText
+//
+//  Created by 王少杰 on 16/6/30.
+//  Copyright © 2016年 Wsj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TJCoreText/TJCoreText/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TJCoreText
+//
+//  Created by 王少杰 on 16/6/30.
+//  Copyright © 2016年 Wsj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
